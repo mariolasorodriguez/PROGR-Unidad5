b3766c09f534d1d59cef2df2f413d57da3060878
Mario <mario.laso@iesnervion.es>
2026-03-24 13:42:35 +0100

    Ejercicio 01

---
+++ ejercicios_tema5/src/ejercicio1/Hora.java
@@ -0,0 +1,50 @@
+package ejercicio1;
+
+public class Hora {
+    private int hora;
+    private int minuto;
+
+    public Hora(int hora, int minuto) {
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+
+    /**
+     * Metodo que incrementa la hora en un minuto.
+     */
+    void incrementar() {
+        if (minuto == 59) {
+            minuto = 0;
+            if (hora == 23) {
+                hora = 0;
+            } else {
+                hora++;
+            }
+        } else {
+            minuto++;
+        }
+    }
+
+    boolean setMinutos(int valor) {
+        this.minuto = valor;
+        if (minuto != valor) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    boolean setHora(int valor) {
+        this.hora = valor;
+        if (valor < 0 || valor > 23) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return + hora + ":" + minuto;
+    }
+}
---
+++ ejercicios_tema5/src/ejercicio1/HoraExacta.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class HoraExacta extends Hora {
+
+    public HoraExacta(int hora, int minuto) {
+        super(hora, minuto);
+    }
+
+
+}
