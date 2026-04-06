b0e3232b0a545626c19cf84a5ebe072180b3275d
mario.laso <mario.laso@iesnervion.es>
2026-04-06 10:04:41 +0200

    Ejercicio 4

---
+++ ejercicios_tema5/src/ejercicio4/Electrodomestico.java
@@ -0,0 +1,10 @@
+package ejercicio4;
+
+public class Electrodomestico {
+    private double precioBase = 100;
+    private String color = "blanco";
+    private char consumo = 'F';
+    private double peso = 5;
+
+
+}
