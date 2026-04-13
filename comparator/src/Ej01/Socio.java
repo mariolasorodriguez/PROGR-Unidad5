c2331ba28879d84029080165cdf9c1566e9ca3f5
Mario <mario.laso@iesnervion.es>
2026-04-13 08:38:24 +0200

    Ejercicio 05

---
+++ comparator/src/Ej01/Socio.java
@@ -0,0 +1,56 @@
+package Ej01;
+
+import java.util.Objects;
+
+public class Socio {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Socio(String nombre, int edad, String apellidos) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + apellidos + ", " + edad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Socio socio = (Socio) o;
+        return edad == socio.edad && Objects.equals(nombre, socio.nombre) && Objects.equals(apellidos, socio.apellidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellidos, edad);
+    }
+}
